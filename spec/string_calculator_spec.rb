760c2a173c867638d4a11112e00e4565a77926ea
Santhosh Ramaiah <santhosh_ramaiah@steris.com>
2024-07-05 02:16:51 +0530

    RED: add test case for various character has a delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -47,7 +47,7 @@ describe StringCalculator do
       end
     end
 
-    context "case: delimeter along with newline '\n' along with ',' in string" do
+    context "case: delimiter along with newline '\n' along with ',' in string" do
       context "given '1\n2,3'" do
         it "returns 6" do
           expect(subject.add("1\n2,3")).to eq(6)
@@ -61,6 +61,20 @@ describe StringCalculator do
       context "given '20,19\n21,33,4,45,0'" do
         it "returns 142" do
           expect(subject.add("20,19\n21,33,4,45,0")).to eq(142)
+        end
+      end
+    end
+
+    context "case: delimiter with various characters '//;\n;- etc' in string" do
+      context "given '//;\n1;2'" do
+        it "returns 3" do
+          expect(subject.add("//;\n1;2")).to eq(3)
+        end
+      end
+
+      context "given '//;\n1;2.4'" do
+        it "returns 3" do
+          expect(subject.add("//;\n1;2.4")).to eq(7)
         end
       end
     end
