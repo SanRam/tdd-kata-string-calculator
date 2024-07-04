d06c98264a65bc07ed00861a470fde10e680cc07
Santhosh Ramaiah <santhosh_ramaiah@steris.com>
2024-07-05 03:15:57 +0530

    GREEN: handle get_called_count to return no of iinvocations of add method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,18 @@
 module StringCalculator
+  # stores the amount of times add method is called
+  @@add_count = 0
+
   def self.add(input)
+    @@add_count = @@add_count + 1
     return 0 if input.nil? || input.empty?
     # this should handle many numbers in a string with various delimiter cases
     numbers = input.scan(/-?\d*/m).map { |num| num.to_i }
     negatives = numbers.select(&:negative?)
     raise "negatives not allowed [#{negatives.join(',')}]" unless negatives.empty?
     numbers.sum
+  end
 
+  def self.get_called_count
+    @@add_count
   end
 end
