943939cac687171f3747554254e0e17abd69dff6
Santhosh Ramaiah <santhosh_ramaiah@steris.com>
2024-07-05 01:47:56 +0530

    RED: add test case for 2 or more numbers and nil

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,20 +2,36 @@ require "string_calculator"
 
 describe StringCalculator do
   describe ".add" do
-    context "I: empty string" do
-      it "O: returns zero" do
+    context "case: empty string" do
+      it "returns zero" do
         expect(subject.add("")).to eq(0)
+      end
+
+      it "returns zero" do
+        expect(subject.add(nil)).to eq(0)
       end
     end
 
-    context "I: one number in string" do
-      it "O: returns one number" do
+    context "case: one number in string" do
+      it "returns one number" do
         expect(subject.add("1")).to eq(1)
       end
-      it "O: returns one number" do
+      it "returns one number" do
         expect(subject.add("6")).to eq(6)
       end
     end
-    
+
+    context "case: two number in string" do
+      context "given '1,2'" do
+        it "returns 3" do
+          expect(subject.add("1,2")).to eq(3)
+        end
+      end
+      context "given '6,19'" do
+        it "returns 25" do
+          expect(subject.add("6,19")).to eq(25)
+        end
+      end
+    end
   end
 end
