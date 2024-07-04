50d79326625cdf99fd6a9229777a8562b112b3dc
Santhosh Ramaiah <santhosh_ramaiah@steris.com>
2024-07-05 01:25:19 +0530

    RED: add empty string test case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,11 @@
 require "string_calculator"
 
 describe StringCalculator do
+  describe ".add" do
+    context "I: empty string" do
+      it "O: returns zero" do
+        expect(subject.add("")).to eq(1)
+      end
+    end
+  end
 end
