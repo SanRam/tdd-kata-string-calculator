a672c76d3aac49b693cd6aafffdacb3990523476
Santhosh Ramaiah <santhosh_ramaiah@steris.com>
2024-07-05 01:54:53 +0530

    GREEN: fix many number case for string-calculator

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,7 @@ module StringCalculator
     if input.nil? || input.empty?
       0
     else
+      # this should handle many numbers in a string with delimiter
       numbers = input.split(",").map { |num| num.to_i }
       numbers.sum
     end
