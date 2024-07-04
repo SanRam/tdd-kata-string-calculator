abc4f4e91a86c2208cd964d041d3c561e641abe8
Santhosh Ramaiah <santhosh_ramaiah@steris.com>
2024-07-05 02:45:18 +0530

    GREEN: handle negative number for string-calculator

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,10 @@
 module StringCalculator
   def self.add(input)
-    if input.nil? || input.empty?
-      0
-    else
-      # this should handle many numbers in a string with various delimiter cases
-      numbers = input.scan(/\d*/).map { |num| num.to_i }
-      numbers.sum
-    end
+    return 0 if input.nil? || input.empty?
+    # this should handle many numbers in a string with various delimiter cases
+    numbers = input.scan(/-?\d*/m).map { |num| num.to_i }
+    raise "negatives not allowed" if numbers.find(&:negative?)
+    numbers.sum
+
   end
 end
